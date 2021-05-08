208bfb04838dcc26ac5d4e28cdba0860aef72fe0
Kaelan Moffett-Steinke <kaelan.ms@gmail.com>
2021-05-08 15:09:24 -0400

    fix stuff

--- frontend/src/pages/resume/generate/index.svelte
+++
@@ -1,1 +0,0 @@
-<h1>no resume generator yet. someone code that pls</h1>
--- frontend/src/pages/resume/input/index.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    import ResumeInputForm from '../../../components/resume_input_form/resume_input_form.svelte';
-    import FullscreenFlexPage from '../../../components/general/fullscreen_flex_page/fullscreen_flex_page.svelte'
-</script>
-
-<FullscreenFlexPage>
-    <ResumeInputForm/>
-</FullscreenFlexPage>
